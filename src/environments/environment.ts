98e3578d6ad67c9d53d2cc9cf383ebbc1ac5214c
NIKHIL VIJAY <42957774+nikhilvijay@users.noreply.github.com>
2026-09-12 20:59:18 +0530

    Production setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ import { Environment } from './environment.model';
 export const environment: Environment = {
   production: true,
   environmentName: 'production',
-  apiUrl: 'https://api.dodgeconstructions.com/api/items',
+  apiUrl: 'https://timely-florentine-2f5598.netlify.app/assets/items',
   enableLogging: false
 };
--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -3,6 +3,6 @@ import { Environment } from './environment.model';
 export const environment: Environment = {
   production: false,
   environmentName: 'uat',
-  apiUrl: 'https://uat-api.dodgeconstructions.com/api/items',
+  apiUrl: 'https://nikhilvijay.com/api/items',
   enableLogging: true
 };
