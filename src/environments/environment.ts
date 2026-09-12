a4ffe2aab3edcce8109f427453fe154794f810a4
NIKHIL VIJAY <42957774+nikhilvijay@users.noreply.github.com>
2026-09-12 21:03:02 +0530

    Fix production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ import { Environment } from './environment.model';
 export const environment: Environment = {
   production: true,
   environmentName: 'production',
-  apiUrl: 'https://timely-florentine-2f5598.netlify.app/assets/items',
+  apiUrl: 'https://timely-florentine-2f5598.netlify.app/assets/items.json',
   enableLogging: false
 };
--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -3,6 +3,6 @@ import { Environment } from './environment.model';
 export const environment: Environment = {
   production: false,
   environmentName: 'uat',
-  apiUrl: 'https://nikhilvijay.com/api/items',
+  apiUrl: 'https://nikhilvijay.com/api/items.json',
   enableLogging: true
 };
