4a37be5710dc903c3b08fd9fdd0629ff46e121c6
NIKHIL VIJAY <42957774+nikhilvijay@users.noreply.github.com>
2026-09-12 21:54:13 +0530

    Added proper change detection

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
     selector: 'app-root',
     imports: [RouterOutlet, RouterLinkActive, RouterLink],
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
     title = 'DCC Coding Sessions';
