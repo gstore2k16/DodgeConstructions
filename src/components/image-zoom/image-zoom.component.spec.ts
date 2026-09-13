2ffcd8c7333db198e982546deec8f8fd365de426
NIKHIL VIJAY <42957774+nikhilvijay@users.noreply.github.com>
2026-09-13 18:47:39 +0530

    Code format test

--- src/components/image-zoom/image-zoom.component.spec.ts
+++ src/components/image-zoom/image-zoom.component.spec.ts
@@ -17,7 +17,7 @@ function makeMouseEvent(
 }
 
 describe('ImageZoomComponent (Jest)', () => {
-  let component: ImageZoomComponent;
+  let component1: ImageZoomComponent;
 
   beforeEach(() => {
     const injector = Injector.create({
@@ -34,7 +34,7 @@ describe('ImageZoomComponent (Jest)', () => {
   it('should initialize with default zoom levels and hover states', () => {
     expect(component.isHovered()).toBe(false);
     expect(component.modalZoom()).toBe(1.2);
-    expect(component.zoomPosition()).not.toEqual({ x: 50, y: 50 });
+    expect(component.zoomPosition()).toEqual({ x: 50, y: 50 });
   });
 
   it('should default alt text to "Product image"', () => {
